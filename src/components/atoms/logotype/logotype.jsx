eeb2949ec862bd3915867f17f928a7c531198db3
Pedro <arpm081001@gmail.com>
2022-03-12 15:04:46 -0600

    Fixing

--- src/components/atoms/logotype/logotype.jsx
+++ src/components/atoms/logotype/logotype.jsx
@@ -1,10 +1,11 @@
 import "./logotype.css";
 import { Link } from "wouter";
+import Logo from "../../../assets/Rick_and_Morty_logo.png"
 
 export default function Logotype() {
   return (
     <Link to="/">
-      <img src="/src/assets/Rick_and_Morty_logo.png" alt="logotype" />
+      <img src={Logo} alt="logotype" />
     </Link>
   );
 }
